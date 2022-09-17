2fbb3f402ca132be695a848db733d713c74886ce
Igor <kugimn@list.ru>
2022-09-17 03:16:13 +0300

    Task4_Мах 3 numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.Write("Введите число А: "); 
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: "); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число С: "); 
+int numberС = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberA > numberB)
+{
+  Console.WriteLine($"Максимальное число - {numberA}, минимальное число - {numberB}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число - {numberB}, минимальное число - {numberA}");    
+}
+
