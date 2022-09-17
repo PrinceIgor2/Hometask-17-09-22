673537beaa20442bd56d2fe018798b6dabb7e2a3
Igor <kugimn@list.ru>
2022-09-17 03:20:26 +0300

    Task2 Task4 ready

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,19 +4,24 @@
 44 5 78 -> 78
 22 3 9 -> 22
 */
-Console.Write("Введите число А: "); 
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: "); 
-int numberB = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число С: "); 
-int numberС = Convert.ToInt32(Console.ReadLine());
-int max = numberA;
-if(numberA > numberB)
-{
-  Console.WriteLine($"Максимальное число - {numberA}, минимальное число - {numberB}");
-}
-else
-{
-    Console.WriteLine($"Максимальное число - {numberB}, минимальное число - {numberA}");    
-}
+Console.WriteLine("Введите первое число: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+
+if (num3 > max) max = num3;
+
+Console.WriteLine($"Максимальное число - {max}");
+
 
